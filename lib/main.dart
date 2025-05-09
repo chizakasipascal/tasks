47c2f2754b7340cd63f41b11636d369da152d6b6
Chiza Kasi P <53052944+chizakasipascal@users.noreply.github.com>
2025-05-09 08:49:55 +0200

    add observation bloc to observer all states and events management;

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,35 @@
+import 'package:blocs/blocs.dart';
 import 'package:flutter/material.dart';
 import 'package:task/src/app.dart';
 
+class MyBlocObserver extends BlocObserver {
+  @override
+  void onEvent(Bloc bloc, Object? event) {
+    debugPrint('Main.MyBlocObserver.OnEvent ::: EVENT: $event');
+    super.onEvent(bloc, event);
+  }
+
+  @override
+  void onTransition(Bloc bloc, Transition transition) {
+    debugPrint('Main.MyBlocObserver.OnTransition ::: TRANSITION: $transition');
+    super.onTransition(bloc, transition);
+  }
+
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    super.onChange(bloc, change);
+    debugPrint('Main.MyBlocObserver.onChange ::: CHANGER: $change');
+  }
+
+  @override
+  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
+    super.onError(bloc, error, stackTrace);
+    debugPrint('Main.MyBlocObserver.OnError ::: '
+        'ERROR: $error & STACKTRACE: $stackTrace');
+  }
+}
+
 void main() {
+  Bloc.observer = MyBlocObserver();
   runApp(const MyApp());
 }
